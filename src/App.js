6efb0ea82fc01f303584d1fd37c1f9e29dd630e2
Samuel Kuriakose <skuriacs@gmail.com>
2021-03-29 04:45:26 -0400

    Finished app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from "react"
-const Hello = (props) =>
+const Header = (prop) =>
 {
   return (
-    <div>
-      <p>Hello World {props.name}, you are {props.age} years old</p>
-    </div>
+    <h1>{prop.course}</h1>
+  )
+}
+
+const Part = (prop) =>
+{
+  return (
+    <p> {prop.name} {prop.num} </p>
+  )
+}
+const Content = (prop) => 
+{
+  return (
+    <>
+      <Part name={prop.course1name} num={prop.course1num} />
+      <Part name={prop.course2name} num={prop.course2num} />
+      <Part name={prop.course3name} num={prop.course3num} />
+    </>
+  )
+}
+
+const Total = (Num) =>
+{
+  return (
+    <>
+      <p>Number of exercises {Num.number}</p>
+    </>
   )
 }
 const App = () =>
@@ -17,19 +41,11 @@ const App = () =>
   const part3 = "State of a component"
   const exercise3 = 14
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
-    </div>
+    <>
+      <Header course={course} />
+      <Content course1name={part1} course2name={part2} course3name={part3} course1num={exercise1} course2num={exercise2} course3num={exercise3} />
+      <Total number={exercise1 + exercise2 + exercise3} />
+    </>
   )
 }
 
