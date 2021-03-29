9e908f7ec9f47aa5f06256def3f08013612f2e9a
Samuel Kuriakose <skuriacs@gmail.com>
2021-03-29 04:21:44 -0400

    First part of the exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react"
+const Hello = (props) =>
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello World {props.name}, you are {props.age} years old</p>
     </div>
-  );
+  )
+}
+const App = () =>
+{
+  const course = "Half Stack application developmenmt"
+  const part1 = "Fundamentals of react"
+  const exercise1 = 10
+  const part2 = "Using props to pass data"
+  const exercise2 = 7
+  const part3 = "State of a component"
+  const exercise3 = 14
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercise1}
+      </p>
+      <p>
+        {part2} {exercise2}
+      </p>
+      <p>
+        {part3} {exercise3}
+      </p>
+      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
