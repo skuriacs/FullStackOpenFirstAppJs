b27cc2385bf6978862abd64682a355a34367ac68
Samuel Kuriakose <skuriacs@gmail.com>
2021-04-03 03:55:42 -0400

    Changed the app to increase and decrease based on button presses

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react"
-
-const App = (props) =>
+import React, { useState } from "react"
+const Display = ({ counter }) => <div>{counter}</div>
+const Button = ({ onTouch, text }) =>
 {
-  const { counter } = props
   return (
-    <div>{counter}</div>
+    <button onClick={onTouch}>
+      {text}
+    </button>
   )
+}
+const App = () =>
+{
+  const [counter, setCounter] = useState(0)
+  const handleClick = () => setCounter(counter + 1)
+  const resetClick = () => setCounter(0)
+  const decreaseClick = () => setCounter(counter - 1)
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onTouch={handleClick} text={"Add one!"} />
+      <Button onTouch={resetClick} text={"Reset to 0!"} />
+      <Button onTouch={decreaseClick} text="Subtract one!" />
+    </div>
+  )
+
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 let counter = 0
 
-const refresh = () => ReactDOM.render(
-  <App counter={counter} />,
-  document.getElementById("root")
-)
-
-setInterval(() =>
-{
-  refresh()
-  counter += 1
-}, 1)
+ReactDOM.render(<App />,
+  document.getElementById("root"))
